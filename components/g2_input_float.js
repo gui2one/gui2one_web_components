e24c1748bc4ea7d65553f3efac1473e1fba9e793
gui2one <guillaume.rouault.fx@gmail.com>
2024-01-09 23:33:05 +0100

    input float label

--- components/g2_input_float.js
+++ components/g2_input_float.js
@@ -15,6 +15,7 @@ export class GuiInputFloat extends HTMLElement {
         this.color = "hotpink";
         this.is_mouse_down = false;
         this.is_dragging = false;
+        this._label = "wtf ?";
         this.styles = String.raw `
             <style>
 
