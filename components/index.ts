ec7a266260e2b6e64ce4c786f6165e539063b9f5
gui2one <guillaume.rouault.fx@gmail.com>
2024-01-24 22:58:07 +0100

    heu shit !!

--- components/index.ts
+++ components/index.ts
@@ -1,12 +1,14 @@
-import "./g2_accordion";
-import "./g2_checkbox";
-import "./g2_collapsible";
-import "./g2_color_picker";
-import "./g2_combobox";
-import "./g2_group";
-import "./g2_input_color";
-import "./g2_input_float";
-import "./g2_input_vector";
-import "./g2_panel";
-import "./g2_separator";
-import "./g2_title";
+import { GuiAccordion } from "./g2_accordion";
+import { GuiCheckbox } from "./g2_checkbox";
+import { GuiCollapsible } from "./g2_collapsible";
+import { GuiColorPicker } from "./g2_color_picker";
+import { GuiCombobox } from "./g2_combobox";
+import { GuiGroup } from "./g2_group";
+import { GuiInputColor } from "./g2_input_color";
+import { GuiInputFloat } from "./g2_input_float";
+import { GuiInputVector } from "./g2_input_vector";
+import { GuiPanel } from "./g2_panel";
+import { GuiSeparator } from "./g2_separator";
+import { GuiTitle } from "./g2_title";
+
+module.exports = [ GuiAccordion, GuiCheckbox, GuiCollapsible, GuiColorPicker, GuiCombobox, GuiGroup, GuiInputColor, GuiInputFloat, GuiInputVector, GuiPanel, GuiSeparator, GuiTitle]
