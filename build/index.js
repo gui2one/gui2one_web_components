49929100b0a8c000b6d35dc92568158d1582b17a
gui2one <guillaume.rouault.fx@gmail.com>
2024-01-28 10:08:36 +0100

    exports again ...

--- build/index.js
+++ build/index.js
@@ -1,3 +1,39 @@
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// components/index.ts
+var components_exports = {};
+__export(components_exports, {
+  GuiAccordion: () => GuiAccordion,
+  GuiCheckbox: () => GuiCheckbox,
+  GuiCollapsible: () => GuiCollapsible,
+  GuiColorPicker: () => GuiColorPicker,
+  GuiCombobox: () => GuiCombobox,
+  GuiGroup: () => GuiGroup,
+  GuiInputColor: () => GuiInputColor,
+  GuiInputFloat: () => GuiInputFloat,
+  GuiInputVector: () => GuiInputVector,
+  GuiPanel: () => GuiPanel,
+  GuiSeparator: () => GuiSeparator,
+  GuiTitle: () => GuiTitle
+});
+module.exports = __toCommonJS(components_exports);
+
 // components/g2_accordion.ts
 var GuiAccordion = class extends HTMLElement {
   template_fragment;
--- components/index.ts
+++ components/index.ts
@@ -1,15 +1,27 @@
 import { GuiAccordion } from "./g2_accordion";
+export { GuiAccordion } from "./g2_accordion";
 import { GuiCheckbox } from "./g2_checkbox";
+export { GuiCheckbox } from "./g2_checkbox";
 import { GuiCollapsible } from "./g2_collapsible";
+export { GuiCollapsible } from "./g2_collapsible";
 import { GuiColorPicker } from "./g2_color_picker";
+export { GuiColorPicker } from "./g2_color_picker";
 import { GuiCombobox } from "./g2_combobox";
+export { GuiCombobox } from "./g2_combobox";
 import { GuiGroup } from "./g2_group";
+export { GuiGroup } from "./g2_group";
 import { GuiInputColor } from "./g2_input_color";
+export { GuiInputColor } from "./g2_input_color";
 import { GuiInputFloat } from "./g2_input_float";
+export { GuiInputFloat } from "./g2_input_float";
 import { GuiInputVector } from "./g2_input_vector";
+export { GuiInputVector } from "./g2_input_vector";
 import { GuiPanel } from "./g2_panel";
+export { GuiPanel } from "./g2_panel";
 import { GuiSeparator } from "./g2_separator";
+export { GuiSeparator } from "./g2_separator";
 import { GuiTitle } from "./g2_title";
+export { GuiTitle } from "./g2_title";
 
 window["GuiAccordion"] = GuiAccordion;
 window["GuiCheckbox"] = GuiCheckbox;
