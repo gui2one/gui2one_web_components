1dde0e9bd23bfa516adb64185b440e3ce5a8ddeb
gui2one <guillaume.rouault.fx@gmail.com>
2024-01-30 21:30:33 +0100

    fail on combo box addOption

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-// import { GuiPanel } from "./build/index.js"
+import { GuiPanel, GuiInputVector, GuiCombobox } from "./build/index.js"
 
 // const { GuiInputVector } = require("./components/g2_input_vector");
 
-// let panel = new GuiPanel();
-// panel.setAttribute("side", "left");
+let panel = new GuiPanel();
+document.body.appendChild(panel);
+panel.setAttribute("side", "left");
 // console.log(panel);
-// let vector = new GuiInputVector();
-// vector.label = "Position";
-// panel.appendChild(vector)
-// document.body.appendChild(panel);
+let vector = new GuiInputVector();
+vector.label = "Position";
+panel.append(vector);
+
+let combo = new GuiCombobox();
+combo.label = "combo test"
+combo.addOption("hello");
+panel.append(combo)
